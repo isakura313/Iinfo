5e33f1b95f375fc383948da84d64c57c12d4f92d
Satana71 <56357249+Satana71@users.noreply.github.com>
2020-03-17 13:08:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// alert('Привет, мир!');
+// alert('Привет, мир 2!');
+// console.log('Привет, мир!');
+
+// $('.button').click(function(){
+//     $('.button').css('background-color', '#ef00ef');
+//     $('.project').css('background-color', '#11ff11');
+//     $('p').css('color', '#ff1111');
+
+// });
+
+$('.button').click(function(){
+    $('.allinfo').css('display', 'block');
+    $('.allinfo').show();
+    
+    $('.allinfo').fadeIn(1000);
+    
+});
